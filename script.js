e19212a11955c16a888c6e5159132318d678a3ec
ELHARFALI <elharfali.youssef.20@gmail.com>
2022-09-03 16:49:17 +0100

    Add Logic To The Game

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
 'use strict';
+
+const check = document.querySelector('.check');
+const message = document.querySelector('.message');
+const number = document.querySelector('.number');
+const labelScore = document.querySelector('.score');
+
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+
+number.textContent = secretNumber;
+
+check.addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value)
+
+    if (!guess) {
+        message.textContent = 'No Number';
+
+        } else if (guess == secretNumber) {
+        message.textContent = 'Correct Number';
+
+    } else if (guess > secretNumber) {
+        
+            if (score > 1) {
+                message.textContent = 'Too High!';
+                score--;
+                labelScore.textContent = score;
+
+            } else {
+                message.textContent = 'You Lose The Game!';
+                labelScore.textContent = 0;
+            }
+        
+    } else if (guess < secretNumber) {
+        
+            if (score > 1) {
+                message.textContent = 'Too Low!';
+                score--;
+                labelScore.textContent = score;
+
+            } else {
+                message.textContent = 'You Lose The Game!';
+                labelScore.textContent = 0;
+        }
+    }
+})
